c5c830ee44ff13780f1129cbc88e396a8e280ecb
yarnall <yarnall@lvc.edu>
2018-09-12 09:50:43 -0400

    la la

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 import java.util.concurrent.atomic.AtomicLongArray;
 
 public class Main {
-
-    private static class VLong {
-        public volatile long value;
-        public long a;
-    }
-
     static volatile long x;
+    //static long a;
     static volatile long y;
-
-    static VLong v1 = new VLong();
-    static VLong v2 = new VLong();
 
     public static void work() {
         x = y  = 0;
@@ -19,7 +11,7 @@ public class Main {
         Thread th1 = new Thread(() -> {
 
             for (int i=0; i<200_000_000; ++i)
-               x += i*i;
+                x += i*i;
 
         });
 
@@ -40,7 +32,7 @@ public class Main {
             th1.join();
             th2.join();
 
-            System.out.println("\t\ttime is: " + (System.nanoTime()-start)/1_000_000_000.0);
+            System.out.println("\t\twork1 time is: " + (System.nanoTime()-start)/1_000_000_000.0);
             System.out.println(x);
             System.out.println(y);
         }
@@ -49,16 +41,19 @@ public class Main {
     }
 
     public static void work2() {
-        v1.value = v2.value = 0;
+        x = y  = 0;
 
         Thread th1 = new Thread(() -> {
+
             for (int i=0; i<200_000_000; ++i)
-                v1.value += i*i;
+                x += i*i;
         });
 
         Thread th2 = new Thread(() -> {
+
             for (int i=0; i<200_000_000; ++i)
-                v2.value += i*i;
+                y += i*i;
+
         });
 
 
@@ -71,21 +66,22 @@ public class Main {
             th1.join();
             th2.join();
 
-            System.out.println("\t\ttime is: " + (System.nanoTime()-start)/1_000_000_000.0);
-            System.out.println(v1.value);
-            System.out.println(v2.value);
+            System.out.println("\t\twork2 time is: " + (System.nanoTime()-start)/1_000_000_000.0);
+            System.out.println(x);
+            System.out.println(y);
         }
         catch (InterruptedException ignore) {
         }
     }
+
 
     public static void main(String[] args) {
         work();
         work();
         work();
 
-        work2();
-        work2();
-        work2();
+//        work2();
+//        work2();
+//        work2();
     }
 }
